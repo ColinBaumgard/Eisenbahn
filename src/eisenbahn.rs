16bcdeca9b09ac2a16cb79e77139c1a8e9a29166
Colin <colin.baumgard@gmail.com>
2023-02-27 19:34:34 +0100

    improved graph

--- src/eisenbahn.rs
+++ src/eisenbahn.rs
@@ -2,7 +2,7 @@ use crate::{layer, mouse, ColorNames, GameColors, MouseState, ORANGE, PURPLE};
 
 use bevy::{
     prelude::*,
-    sprite::{Material2d, MaterialMesh2dBundle},
+    sprite::{Material2d, MaterialMesh2dBundle}, math,
 };
 
 use bevy_prototype_lyon::{entity::*, prelude::*};
@@ -27,23 +27,20 @@ fn initialise_world(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let p1 = Vec2 { x: 100.0, y: 50.0 };
-    let p2 = Vec2 {
-        x: -100.0,
-        y: -50.0,
-    };
-    let e1 = add_track_node(&mut commands, p1, &mut graph.0);
-    let e2 = add_track_node(&mut commands, p2, &mut graph.0);
-
-    graph.0.add_edge(e1, e2, 0.0);
-
-    commands.spawn((
-        Track { e1: e1, e2: e2 },
-        GeometryBuilder::build_as(
-            &shapes::Line(p1, p2),
-            DrawMode::Stroke(StrokeMode::new(ORANGE, 2.0)),
-            Transform::from_xyz(0.0, 0.0, layer::GROUND),
-        ),
-    ));
+    let p2 = Vec2 { x: -100.0, y: -50.0 };
+    let p3 = Vec2 { x: 34.0, y: -50.0 };
+    let p4 = Vec2 { x: 200.0, y: 60.0 };
+    let p5 = Vec2 { x: -70.0, y: 27.0 };
+    let e1 = add_node(&mut commands, p1, &mut graph.0);
+    let e2 = add_node(&mut commands, p2, &mut graph.0);
+    let e3 = add_node(&mut commands, p3, &mut graph.0);
+    let e4 = add_node(&mut commands, p4, &mut graph.0);
+    let e5 = add_node(&mut commands, p5, &mut graph.0);
+    add_edge(&mut commands, e1, p1, e2, p2, &mut graph.0);
+    add_edge(&mut commands, e2, p2, e3, p3, &mut graph.0);
+    add_edge(&mut commands, e5, p5, e3, p3, &mut graph.0);
+    add_edge(&mut commands, e1, p1, e5, p5, &mut graph.0);
+    
 }
 
 #[derive(Resource)]
@@ -61,7 +58,28 @@ pub struct Track {
     e2: Entity,
 }
 
-fn add_track_node(
+
+fn add_edge(
+    commands: &mut Commands,
+    entity1: Entity,
+    position1: Vec2,
+    entity2: Entity,
+    position2: Vec2,
+    graph: &mut GraphMap<Entity, f32, Undirected>,
+) -> Entity{
+    graph.add_edge(entity1, entity2, position1.distance(position2));
+
+    commands.spawn((
+        Track { e1: entity1, e2: entity2 },
+        GeometryBuilder::build_as(
+            &shapes::Line(position1, position2),
+            DrawMode::Stroke(StrokeMode::new(ORANGE, 2.0)),
+            Transform::from_xyz(0.0, 0.0, layer::GROUND),
+        ),
+    )).id()
+}
+
+fn add_node(
     commands: &mut Commands,
     position: Vec2,
     graph: &mut GraphMap<Entity, f32, Undirected>,
@@ -81,7 +99,7 @@ fn add_track_node(
     graph.add_node(entity);
     entity
 }
-fn remove_track_node(
+fn remove_node(
     commands: &mut Commands,
     entity: Entity,
     graph: &mut GraphMap<Entity, f32, Undirected>,
